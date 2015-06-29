871d23feb9cd66c38527ef705c7dea7b2afb28bd
Jiamao Zheng <jiamaoz@yahoo.com>
2015-06-28 20:44:05 -0500

    finish 3_weather

--- 3_weather.rb
+++ 3_weather.rb
@@ -14,12 +14,12 @@ json_data = open("http://api.openweathermap.org/data/2.5/weather?q=#{location}&u
 # Parse the response string into a Ruby data structure
 # (You will need to figure out what kind of structure it is)
 data = JSON.parse(json_data)
-
+puts data
 
 # 1. TO DO:
 # Replace the following 0 with an expression
 # that will extract the temperature
-temp = 0
+temp = data["main"]["temp"]
 
 # Output the temperature to the screen
 puts "It is currently #{temp.round(0)}\u00B0 outside."
