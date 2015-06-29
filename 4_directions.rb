8a4157ac11df93bde6dde2e312478f79de9e7768
Jiamao Zheng <jiamaoz@yahoo.com>
2015-06-28 21:03:25 -0500

    4_directions

--- 4_directions.rb
+++ 4_directions.rb
@@ -13,22 +13,23 @@ destination = URI.escape(destination)
 url = "http://maps.googleapis.com/maps/api/directions/json?origin=#{origin}&destination=#{destination}&sensor=false"
 puts url
 json_data = open(url).read
+# puts json_data
 
 # Parse the response string into a Ruby data structure
 # (You will need to figure out what kind of structure it is)
 data = JSON.parse(json_data)
-
+# puts data 
 
 # 1. TO DO:
 # Replace the following 0 with an expression that will retrieve the total travel time
-total_time = 0
+total_time = data["routes"][0]["legs"][0]["duration"]["text"]
 
 # Output the total drive time to the screen
 puts "Total travel time driving: #{total_time}"
 
 # 2. TO DO:
 # Replace the following 0 with an expression that will retrieve the total distance
-total_distance = 0
+total_distance = data["routes"][0]["legs"][0]["distance"]["text"]
 
 # Output the total distance to the screen
 puts "Total distance traveled: #{total_distance}"
